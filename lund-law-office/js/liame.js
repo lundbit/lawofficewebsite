742d5ffac67ac60ffa146b8ea92532a448ea99e4
Brad Lund <32400736+lundbit@users.noreply.github.com>
2018-12-22 11:06:15 -0700

    Add files via upload

---
+++ lund-law-office/js/liame.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+function liame(naam, domain, tld, label) {
+    var mail;
+    mail += '<a href="' + 'ma' + 'il' + 'to:' + naam;
+    mail += '@' + mail + domain '.' + tld;
+    mail += '">' + label + '<' + '/a>';
+    document.write(mail);
+}
+</script>
